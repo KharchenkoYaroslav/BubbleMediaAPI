f30754539115d609bc1ace992dc61ab8b12b1718
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2025-05-18 17:27:27 +0300

    fix

--- apps/AuthService/src/main.ts
+++ apps/AuthService/src/main.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  await NestFactory.createMicroservice(AppModule, {
+  const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.GRPC,
     options: {
       package: 'auth',
@@ -13,6 +13,8 @@ async function bootstrap() {
       url: `${process.env.AUTH_SERVICE_URL || '0.0.0.0:3001'}`,
     },
   });
+
+   await app.listen();
 
   Logger.log(
     `🚀 Application is running on: ${process.env.AUTH_SERVICE_URL || 'http://localhost:3001'}`
